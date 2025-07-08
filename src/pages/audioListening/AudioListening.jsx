333e4b3406a1d0cf55312205856919c7f63452e7
Kamoliddin1701313 <kamol170professor@gmail.com>
2025-07-08 12:00:12 +0500

    sayt tayyor bo'ldi bugun

--- src/pages/audioListening/AudioListening.jsx
+++ src/pages/audioListening/AudioListening.jsx
@@ -1,7 +1,94 @@
+import { useEffect, useState } from "react";
+import { FaPlay } from "react-icons/fa6";
 import style from "./audioListening.module.scss";
+import { Fade } from "react-awesome-reveal";
+import axios from "axios";
 
 function AudioListening() {
-  return <div>audioListening.module.scss</div>;
+  const [data, setData] = useState([]);
+  const [clickAudio, setClickAudio] = useState(null);
+
+  const getData = async () => {
+    try {
+      const respons = await axios.get("audiolar/");
+      setData(respons);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const ClickAudio = (id) => {
+    setClickAudio(id);
+  };
+
+  console.log(clickAudio, "clickAudio");
+
+  return (
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <div className={style.menu_link}>
+          <button onClick={() => navigate("/")}>Bosh sahifa</button>
+          <span>/</span>
+
+          <button onClick={() => navigate("/")}>Eshituvlar</button>
+          <span>/</span>
+
+          <button>
+            {clickAudio == null
+              ? data?.data?.results[0]?.title
+              : clickAudio.title}
+          </button>
+        </div>
+
+        <div className={style.audio_container}>
+          <div className={style.active_audio}>
+            <Fade cascade damping={0.2}>
+              <div className={style.img}>
+                <img
+                  src={
+                    clickAudio == null
+                      ? data?.data?.results[0]?.image
+                      : clickAudio.image
+                  }
+                  alt={
+                    clickAudio == null
+                      ? data?.data?.results[0]?.title
+                      : clickAudio.title
+                  }
+                />
+              </div>
+
+              {data?.data?.results[0]?.audio && (
+                <audio controls className={style.audio}>
+                  <source
+                    src={data?.data?.results[0]?.audio}
+                    type="audio/mpeg"
+                  />
+                </audio>
+              )}
+            </Fade>
+          </div>
+
+          <div className={style.audio_wrapper}>
+            <div className={style.audio_btn}>
+              {data?.data?.results?.map((value, index) => (
+                <div key={index} className={style.card}>
+                  <button onClick={() => ClickAudio(value)}>
+                    <FaPlay />
+                    <span>{value?.title}</span>
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AudioListening;
--- src/pages/videoViews/VideoViews.jsx
+++ src/pages/videoViews/VideoViews.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import style from "./videoViews.module.scss";
 import axios from "axios";
+import { Fade } from "react-awesome-reveal";
 
 function VideoViews() {
   const [data, setData] = useState([]);
+  const [activeVideo, setActiveVideo] = useState(null);
 
   const getData = async () => {
     try {
@@ -18,7 +20,9 @@ function VideoViews() {
     getData();
   }, []);
 
-  console.log(data, "video");
+  const ActiveVideoBtn = (id) => {
+    setActiveVideo(id);
+  };
 
   return (
     <div className={style.container}>
@@ -30,7 +34,30 @@ function VideoViews() {
           <button onClick={() => navigate("/")}>Koʻruvlar</button>
           <span>/</span>
 
-          <button></button>
+          <button>
+            {activeVideo == null ? data[0]?.title : activeVideo?.title}
+          </button>
+        </div>
+
+        <div className={style.video_container}>
+          <Fade cascade damping={0.2} className={style.video}>
+            <iframe
+              src={activeVideo == null ? data[0]?.link : activeVideo?.link}
+            ></iframe>
+          </Fade>
+
+          <div className={style.videos_wrapper}>
+            <div className={style.videos_btn}>
+              {data?.map((value, index) => (
+                <div key={index} className={style.card}>
+                  <button onClick={() => ActiveVideoBtn(value)}>
+                    <img src={value?.file} alt={value?.title} />
+                    <span>{value?.title}</span>
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
